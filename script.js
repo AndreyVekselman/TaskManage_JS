7e4187264b94ac02248720051c19bd19764f9a17
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-06 11:06:47 +0200

    check

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ import { Task } from "./Task.mjs";
 
 let Task1 = new Task("create a class", "create 1-st class and use module");
 Task1.showTask();
+const Task2 = new Task("create function showOnHTML", "show it on HTML");
 ////
 showOnHTML(Task1);
+showOnHTML(Task2);
 
+//outputting tasks to an HTML page
 function showOnHTML(task) {
   let mainDiv = document.createElement("div");
   let taskName = document.createElement("div");
