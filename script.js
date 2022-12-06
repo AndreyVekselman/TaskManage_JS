11c48f56a6cc044afa43d5a1feef4c7bd1691197
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-06 10:28:47 +0200

    create function showOnHTML

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ import { Task } from "./Task.mjs";
 
 let Task1 = new Task("create a class", "create 1-st class and use module");
 Task1.showTask();
+////
+showOnHTML(Task1);
+
+function showOnHTML(task) {
+  let mainDiv = document.createElement("div");
+  let taskName = document.createElement("div");
+  let taskText = document.createElement("div");
+  taskName.innerHTML = task.taskName;
+  taskText.innerHTML = task.taskText;
+  mainDiv.appendChild(taskName);
+  mainDiv.appendChild(taskText);
+  document.body.appendChild(mainDiv);
+}
