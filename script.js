c31bca82cf05a240d1390636a944ff2a87762b5a
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-06 12:32:17 +0200

    add footer also to CSS

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ import { Task } from "./Task.mjs";
 let Task1 = new Task("create a class", "create 1-st class and use module");
 Task1.showTask();
 const Task2 = new Task("create function showOnHTML", "show it on HTML");
+
+const Task3 = new Task(
+  "Create CSS file",
+  "Create CSS file and stylish tasks on HTML page"
+);
 ////
 showOnHTML(Task1);
 showOnHTML(Task2);
+showOnHTML(Task3);
 
 //outputting tasks to an HTML page
 function showOnHTML(task) {
+  let main = document.querySelector("main");
   let mainDiv = document.createElement("div");
   mainDiv.className = "mainDiv";
   let taskName = document.createElement("div");
@@ -20,5 +27,5 @@ function showOnHTML(task) {
   taskText.innerHTML = task.taskText;
   mainDiv.appendChild(taskName);
   mainDiv.appendChild(taskText);
-  document.body.appendChild(mainDiv);
+  main.appendChild(mainDiv);
 }
