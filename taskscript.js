f222ae6831cb2e91fa6abaa2dc2134e7f8fd07b5
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-09 14:07:06 +0200

    add subject and text check value

--- taskscript.js
+++ taskscript.js
@@ -8,10 +8,18 @@ NewTask = JSON.parse(LS.getItem("student"));
 const createTaskBtn = document.getElementById("Btn_OK");
 createTaskBtn.addEventListener("click", () => {
   const taskSubject = document.getElementById("taskSubject");
-  const taskText = document.getElementById("taskText");
-  const myTask = new Task();
-  myTask.taskName = taskSubject.value;
-  myTask.taskText = taskText.value;
-  NewTask.studentTasks.push(myTask);
-  LS.setItem("student", JSON.stringify(NewTask));
+  if (taskSubject.value.length > 5) {
+    const taskText = document.getElementById("taskText");
+    if (taskText.value.length > 10) {
+      const myTask = new Task();
+      myTask.taskName = taskSubject.value;
+      myTask.taskText = taskText.value;
+      NewTask.studentTasks.push(myTask);
+      LS.setItem("student", JSON.stringify(NewTask));
+    } else {
+      alert("enter text, at least 10 characters");
+    }
+  } else {
+    alert("enter Subject, at least 5 characters");
+  }
 });
