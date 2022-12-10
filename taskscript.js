6de9410f771f324c3008f6fea7d286f83e83e0fb
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-10 16:05:52 +0200

    add deadline time

--- taskscript.js
+++ taskscript.js
@@ -13,14 +13,20 @@ createTaskBtn.addEventListener("click", () => {
   if (taskSubject.value.length > 5) {
     const taskText = document.getElementById("taskText");
     if (taskText.value.length > 10) {
-      const myTask = new Task();
-      myTask.taskName = taskSubject.value;
-      myTask.taskText = taskText.value;
-      myTask.taskId = NewTask.studentTasks.length;
-      NewTask.studentTasks.push(myTask);
-      LS.setItem("student", JSON.stringify(NewTask));
-      // check point
-      myTask.showTask();
+      const taskDeadLineTime = document.getElementById("taskDate");
+      if (taskDeadLineTime.value) {
+        const myTask = new Task();
+        myTask.taskName = taskSubject.value;
+        myTask.taskText = taskText.value;
+        myTask.taskId = NewTask.studentTasks.length;
+        myTask.taskDeadlineTime = taskDeadLineTime.value;
+        NewTask.studentTasks.push(myTask);
+        LS.setItem("student", JSON.stringify(NewTask));
+        // check point
+        myTask.showTask();
+      } else {
+        alert("enter correct Date for Dead line for submission");
+      }
     } else {
       alert("enter text, at least 10 characters");
     }
