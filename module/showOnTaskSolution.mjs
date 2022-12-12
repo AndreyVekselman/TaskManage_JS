047205d4df84ce039a92ff9cf72c0ff5237bc429
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-12 19:56:25 +0200

    add check Null in local storage

--- module/showOnTaskSolution.mjs
+++ module/showOnTaskSolution.mjs
@@ -5,19 +5,34 @@ import { loadFromLocalStorage } from "./loadFromLocalStorage.mjs";
 import { saveToLocalStorage } from "./saveToLocalStorage.mjs";
 
 function showOnTaskSolution() {
-  const localSudent = loadFromLocalStorage();
-  console.log(localSudent.studentTasks);
-  localSudent.studentTasks.forEach((task) => {
-    if (task.flagSolution) {
-      const txtDiv = document.getElementById("text");
-      const txtNode = document.createTextNode(task.taskText);
-      txtDiv.appendChild(txtNode);
-      const marker = task.taskId;
-    }
-  });
-  // const btn=document.getElementById('btn');
-  // const submitSolutionBtn = document.getElementById("submitSolution");
-  // const link = document.createElement("a");
-  // link.setAttribute("href", "/index.html");
+  if (loadFromLocalStorage()) {
+    const localStudent = loadFromLocalStorage();
+
+    let marker;
+    console.log(localStudent.studentTasks);
+    localStudent.studentTasks.forEach((task) => {
+      if (task.flagSolution) {
+        const txtDiv = document.getElementById("text");
+        const txtNode = document.createTextNode(task.taskText);
+        txtDiv.appendChild(txtNode);
+        marker = task.taskId;
+        task.flagSolution = false;
+      }
+    });
+    const submitSolutionBtn = document.getElementById("submitSolution");
+    submitSolutionBtn.addEventListener("click", () => {
+      const taskSolution = document.getElementById("taskSolution");
+      console.log(taskSolution.value);
+      console.log(taskSolution.value.length);
+      if (taskSolution.value.length > 5) {
+        localStudent.studentTasks[marker].taskTextSolution = taskSolution.value;
+        localStudent.studentTasks[marker].flagComplete = true;
+      } else {
+        alert("enter solution text, at least 5 characters");
+      }
+      console.log(localStudent.studentTasks[marker].taskSolution);
+      saveToLocalStorage(localStudent);
+    });
+  }
 }
 showOnTaskSolution();
