8f314fe1a5441ff97dd5515509f2a747c5adf3e5
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-13 12:49:33 +0200

    modify Student class

--- Student.mjs
+++ Student.mjs
@@ -1,15 +1,25 @@
 export class Student {
-  studentName;
-  studentID;
+  // studentName;
+  // studentID;
+  // studentTasks;
+  // studentGrade;
   studentTasks;
-  studentGrade;
   constructor() {
-    this.studentName;
-    this.studentID;
-    this.studentGrade;
+    // this.studentName;
+    // this.studentID;
+    // this.studentGrade;
     this.studentTasks = [];
+    this.saveToLocalStorage();
   }
   addTask(task) {
     this.studentTasks.push(task);
+  }
+  loadFromLocalStorage() {
+    this.studentTasks = JSON.parse(localStorage("studentTasks"));
+    return this.studentTasks;
+  }
+  saveToLocalStorage(task) {
+    this.addTask(task);
+    localStorage.setItem("studentTasks", JSON.stringify(this.studentTasks));
   }
 }
