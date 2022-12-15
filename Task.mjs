899788448d94192ebc8bb893b598824beec0397b
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-16 00:36:20 +0200

    add variables to task

--- Task.mjs
+++ Task.mjs
@@ -2,13 +2,16 @@ export class Task {
   taskId;
   flagComplete;
   flagSolution;
+  flagCheck;
   taskSubject;
   taskText;
   taskGrade;
   taskDeadlineTime;
   taskTextSolution;
+  taskTeacherNotes;
   constructor() {
     this.taskText;
+    this.flagCheck = false;
     this.flagComplete = false;
     this.flagSolution = false;
     this.taskGrade = 0;
@@ -16,6 +19,7 @@ export class Task {
     this.taskId;
     this.taskDeadlineTime;
     this.taskTextSolution = "";
+    this.taskTeacherNotes = "";
   }
   showTask() {
     console.log(
