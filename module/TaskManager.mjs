c4d87b8a872fe28e9f6622a3cc09f19e039f1065
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-18 00:31:36 +0200

    create a new TaskManager class

---
+++ module/TaskManager.mjs
@@ -0,0 +1,18 @@
+import { Student } from "../Student.mjs";
+import { Task } from "../Task.mjs";
+export class TasksManager {
+  studentTasks;
+  constructor() {
+    this.getAllTasks();
+  }
+  addNewTask(task) {
+    // const newTask = new Task();
+    const newStudent = new Student();
+    newStudent.addTask(task);
+  }
+  getAllTasks() {
+    const newStudent = new Student();
+    this.studentTasks = newStudent.loadFromLocalStorage();
+    return this.studentTasks;
+  }
+}
