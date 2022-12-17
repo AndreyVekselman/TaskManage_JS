2564b68ac73e5e51a00fdb5c55b6920b79f33895
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-17 21:33:07 +0200

    make some beauty

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 // import classes and functions
-import { Task } from "./Task.mjs";
-import { showOnHTML } from "./module/showOnHTML.mjs";
-import { Student } from "./Student.mjs";
-import { removeItems } from "./module/removeItems.mjs";
 import { freshMainScreen } from "./module/freshMainScreen.mjs";
 
 //add or remove tasks on screen when it was added
