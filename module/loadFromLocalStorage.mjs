33295ae242e0d6fd52bf64ba70c52bceda3dc153
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-18 20:58:08 +0200

    delete not useble function

--- module/loadFromLocalStorage.mjs
+++
@@ -1,5 +0,0 @@
-export function loadFromLocalStorage() {
-  const LS = localStorage;
-  const object = JSON.parse(LS.getItem("student"));
-  return object;
-}
--- module/saveToLocalStorage.mjs
+++
@@ -1,4 +0,0 @@
-export function saveToLocalStorage(object) {
-  const LS = localStorage;
-  LS.setItem("student", JSON.stringify(object));
-}
