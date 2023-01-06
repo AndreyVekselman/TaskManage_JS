127d1a6183234df8bb3170d92909c7b8db53e728
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-01-06 21:05:11 +0200

    changed checkDate function

--- module/checkDate.mjs
+++ module/checkDate.mjs
@@ -1,10 +1,13 @@
 export function checkDate(date) {
   const getDate = new Date();
-  const getDateStr =
-    getDate.getFullYear() +
-    "-" +
-    Number(getDate.getMonth() + 1) +
-    "-" +
-    getDate.getDate();
+  let monthStr = Number(getDate.getMonth()) + 1;
+  let dayStr = Number(getDate.getDate());
+  if (monthStr < 10) {
+    monthStr = "0" + monthStr;
+  }
+  if (dayStr < 10) {
+    dayStr = "0" + dayStr;
+  }
+  const getDateStr = getDate.getFullYear() + "-" + monthStr + "-" + dayStr;
   return date >= getDateStr;
 }
